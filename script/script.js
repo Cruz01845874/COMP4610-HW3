a7b2c4088ae450077b159b4f54e823117973d91c
Roberto Cruz <Roberto_Cruz@student.uml.edu>
2022-10-26 12:30:20 -0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var row_begin = document.getElementById('row_begin').value;
+var row_end = document.getElementById('row_end').value;
+var col_begin = document.getElementById('col_begin').value;
+var col_end = document.getElementById('col_end').value;
+
