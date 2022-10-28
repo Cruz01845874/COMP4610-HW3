abd8979bd3324e160828453576cd2bab62ae953e
Cruz01845874 <Roberto_Cruz@student.uml.edu>
2022-10-28 15:34:03 -0400

    added multiplication table (JS)

--- script/script.js
+++ script/script.js
@@ -1,5 +1,25 @@
-var row_begin = document.getElementById('row_begin').value;
-var row_end = document.getElementById('row_end').value;
-var col_begin = document.getElementById('col_begin').value;
-var col_end = document.getElementById('col_end').value;
+var row_begin, row_end, col_begin, col_end;
+
+function createTable() {
+    var output = "<table>";
+    var i, j;
+
+    row_begin = document.getElementById('row_begin').value;
+    row_end   = document.getElementById('row_end').value;
+    col_begin = document.getElementById('col_begin').value;
+    col_end   = document.getElementById('col_end').value;
+
+    for (i = row_begin; i <= row_end; i++) {
+        output += "<tr>";
+
+        for (j = col_begin; j <= col_end; j++) {
+            output = output + "<td>" + (i * j) + "</td>";
+        }
+
+        output += "</tr>";
+    }
+
+    output += "</table>";
+    document.getElementById('multTab').innerHTML = output;
+}
 
