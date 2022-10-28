ca6a1fb1ac13188d284a4b1dda452c239c2c890a
Cruz01845874 <Roberto_Cruz@student.uml.edu>
2022-10-28 17:28:54 -0400

    added css

--- script/script.js
+++ script/script.js
@@ -1,18 +1,25 @@
 var row_begin, row_end, col_begin, col_end;
 
-function createTable() {
+function createTable(row_begin, row_end, col_begin, col_end) {
     var output = "<table>";
+    var message = "Please enter a number between -50 and 50."
     var i, j;
 
-    row_begin = document.getElementById('row_begin').value;
-    row_end   = document.getElementById('row_end').value;
-    col_begin = document.getElementById('col_begin').value;
-    col_end   = document.getElementById('col_end').value;
+    row_begin = Number(document.getElementById('row_begin').value);
+    row_end   = Number(document.getElementById('row_end').value);
+    col_begin = Number(document.getElementById('col_begin').value);
+    col_end   = Number(document.getElementById('col_end').value);
+
+    if (row_begin < -50 || row_end > 50 || col_begin < -50 || col_end > 50) {
+        document.getElementById("error").innerHTML = message;
+        return;
+    }
 
     for (i = row_begin; i <= row_end; i++) {
         output += "<tr>";
 
         for (j = col_begin; j <= col_end; j++) {
+
             output = output + "<td>" + (i * j) + "</td>";
         }
 
