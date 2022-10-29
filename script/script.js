4add5887336a8f61be0e436a3f5aa6e5a4f07255
Cruz01845874 <Roberto_Cruz@student.uml.edu>
2022-10-28 21:25:21 -0400

    added error checking

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ var row_begin, row_end, col_begin, col_end;
 
 function createTable(row_begin, row_end, col_begin, col_end) {
     var output = "<table><span style=\"color: #4e23c4\"><th>&#215;</th></span>";
-    var message = "Please enter a number between -50 and 50."
     var i, j;
 
     row_begin = Number(document.getElementById('row_begin').value);
@@ -10,28 +9,47 @@ function createTable(row_begin, row_end, col_begin, col_end) {
     col_begin = Number(document.getElementById('col_begin').value);
     col_end   = Number(document.getElementById('col_end').value);
 
-    if (row_begin < -50 || row_end > 50 || col_begin < -50 || col_end > 50) {
-        document.getElementById("error").innerHTML = message;
-        return;
-    }
-
-    for (i = row_begin; i <= row_end; i++) {
-        output += "<th>" + i + "</th>";
-    }
-
-    for (i = col_begin; i <= col_end; i++) {
-        output += "<tr>";
-        output += "<th>" + i + "</th>";
-
-        for (j = row_begin; j <= row_end; j++) {
-
-            output = output + "<td>" + (i * j) + "</td>";
+    if (checkArgs(row_begin, row_end, col_begin, col_end)) {
+        for (i = row_begin; i <= row_end; i++) {
+            output += "<th>" + i + "</th>";
         }
-
-        output += "</tr>";
+    
+        for (i = col_begin; i <= col_end; i++) {
+            output += "<tr>";
+            output += "<th>" + i + "</th>";
+    
+            for (j = row_begin; j <= row_end; j++) {
+    
+                output = output + "<td>" + (i * j) + "</td>";
+            }
+    
+            output += "</tr>";
+        }
+    
+        output += "</table>";
+        document.getElementById('multTab').innerHTML = output;
     }
-
-    output += "</table>";
-    document.getElementById('multTab').innerHTML = output;
+    
 }
 
+function checkArgs(r1, r2, c1, c2) {
+    var message;
+    var values = [r1, r2, c1, c2];
+    var i;
+
+    for (i = 0; i < values.length; i++) {
+        if (values[i] == NaN) {
+            message = "Please only enter numbers."
+            document.getElementById("error").innerHTML = message;
+            return false;
+        }
+    }
+
+    if (r1 < -50 || r2 > 50 || c1 < -50 || c2 > 50) {
+        message = "Please enter a number between -50 and 50."
+        document.getElementById("error").innerHTML = message;
+        return false;
+    }
+
+    return true;
+}
